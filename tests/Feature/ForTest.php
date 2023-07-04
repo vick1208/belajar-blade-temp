0d4ef36ed9ab59057b221401c7d13df120a5437c
Vicky AS <vickyhuang808@gmail.com>
2023-07-04 16:37:25 +0700

    edit fortest

--- tests/Feature/ForTest.php
+++ tests/Feature/ForTest.php
@@ -10,17 +10,12 @@ class ForTest extends TestCase
 {
     public function testFor()
     {
-        $this->view("for", ["limit" => 10])
+        $this->view("for", ["limit" => 5])
             ->assertSeeText("0")
             ->assertSeeText("1")
             ->assertSeeText("2")
             ->assertSeeText("3")
-            ->assertSeeText("4")
-            ->assertSeeText("5")
-            ->assertSeeText("6")
-            ->assertSeeText("7")
-            ->assertSeeText("8")
-            ->assertSeeText("9");
+            ->assertSeeText("4");
     }
     public function testForEach()
     {
