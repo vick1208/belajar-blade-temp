e447b88591cc3e2e11443e59c6260686ae0055f0
Vicky AS <vickyhuang808@gmail.com>
2023-07-04 19:59:40 +0700

    css class #️⃣

---
+++ tests/Feature/ClasscssTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ClasscssTest extends TestCase
+{
+    public function testClass()
+    {
+        $this->view("class-css", [
+            "hobbies" => [
+                [
+                    "name" => "Painting",
+                    "love" => true
+                ],
+                [
+                    "name" => "Coding",
+                    "love" => true
+                ],
+                [
+                    "name" => "Gaming",
+                    "love" => false
+                ],
+            ]
+        ])
+            ->assertSee('<li class="red bold">Coding</li>', false)
+            ->assertSee('<li class="red">Gaming</li>', false);
+    }
+}
