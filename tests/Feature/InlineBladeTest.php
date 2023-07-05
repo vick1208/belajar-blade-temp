3213c8ca0532ddc16622515dab632b557a4a0f0f
Vicky AS <vickyhuang808@gmail.com>
2023-07-05 08:37:41 +0700

    Inline Test

--- tests/Feature/InlineBladeTest.php
+++ tests/Feature/InlineBladeTest.php
@@ -4,17 +4,15 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Blade;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
 
 class InlineBladeTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+    public function testInline(){
+        $response = Blade::render('Hello {{ $name }}',['name' => "Eko"]);
+        assertEquals("Hello Eko",$response);
     }
 }
